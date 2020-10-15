eac228735d3f580bea383bb07725dc6e2cb33266
DavidSchmidtSBU <ace.schmidt5@gmail.com>
2020-10-15 13:29:41 -0500

    Add files via upload

---
+++ List v1.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+
+# This is just the starter code for all of the lists. There could be more or less than three items depending on which list you are in. 
+
+# This is the code for listing equipment needing to be sanitized.
+<ul style="list-style-type:none;">
+  <li>Equipment</li>  # This would have the name of the piece of equipment.
+  <li>Equipment</li>  # This would have the name of the piece of equipment.
+  <li>Equipment</li>  # This would have the name of the piece of equipment.
+</ul>
+
+# This is the code for listing equipment that is damaged.
+<ul style="list-style-type:none;">
+  <li>Equipment</li>  # This would have the name of the piece of equipment.
+  <li>Equipment</li>  # This would have the name of the piece of equipment.
+  <li>Equipment</li>  # This would have the name of the piece of equipment.
+</ul>
+
+# This is the code for listing workers that are currently in the system.
+<ul style="list-style-type:none;">
+  <li>Worker</li>  # This would have the name of the worker.
+  <li>Worker</li>  # This would have the name of the worker.
+  <li>Worker</li>  # This would have the name of the worker.
+</ul>
+
+# This is the code for listing all of the equipment.
+<ul style="list-style-type:none;">
+  <li>Equipment</li>  # This would have the name of the piece of equipment.
+  <li>Equipment</li>  # This would have the name of the piece of equipment.
+  <li>Equipment</li>  # This would have the name of the piece of equipment.
+</ul>
+
+?>
+</body>
+</html>
