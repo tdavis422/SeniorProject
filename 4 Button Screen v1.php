7bd7cd56fbe05a80e2142d3e7dd74a59101daf3d
DavidSchmidtSBU <ace.schmidt5@gmail.com>
2020-10-20 17:03:41 -0500

    Update 4 Button Screen v1.php

--- 4 Button Screen v1.php
+++ 4 Button Screen v1.php
@@ -4,27 +4,19 @@
 
 <!--This is the button for Create Report.-->
 <h2>Create Report</h2>
-<form action="Create.Report" method="post">
-<input type="text" report="report"><br> 
-</form>
+<button type="button">Create Report</button>
 
 <!--This is the button for View Damaged.-->
 <h2>View Damaged</h2>
-<form action="View.Damaged" method="post">
-<input type="text" view="view"><br> 
-</form>
+<button type="button">View Damaged</button>
 
 <!--This is the button for Add/Remove.-->
 <h2>Add/Remove</h2>
-<form action="Add./.Remove" method="post">
-<input type="text" add/remove="add/remove"><br> 
-</form>
+<button type="button">Add/Remove</button>
 
 <!--This is the button for Clean Database.-->
 <h2>Clean Database</h2>
-<form action="Clean.Database" method="post">
-<input type="text" clean="clean"><br> 
-</form>
+<button type="button">Clean Database</button>
 
 </body>
 </html>
