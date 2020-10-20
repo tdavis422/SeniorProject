a62a2bc3405d622458720d812d160d3ef58e8024
DavidSchmidtSBU <ace.schmidt5@gmail.com>
2020-10-20 17:35:58 -0500

    Update 2 Button Screen v1.php

--- 2 Button Screen v1.php
+++ 2 Button Screen v1.php
@@ -4,11 +4,11 @@
 
 <!--This is the button for viewing the equipment that needs sanitized.-->
 <h2>View Equipment Needing Sanitized</h2>
-<button type="button">Sanitize</button>
+<button type="button">View Equipment Needing To Be Sanitized</button>
 
 <!--This is the button for labeling an item as damaged.-->
 <h2>Label Item As Damaged</h2>
-<button type="button">Damaged</button>
+<button type="button">Label Item Damaged</button>
 
 </body>
 </html>
