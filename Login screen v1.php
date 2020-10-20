48a8dad7d9a0e65a8830822ee62db03906038286
DavidSchmidtSBU <ace.schmidt5@gmail.com>
2020-10-20 17:17:05 -0500

    Update Login screen v1.php

--- Login screen v1.php
+++ Login screen v1.php
@@ -4,21 +4,15 @@
 
 <!--This is the Student button.-->
 <h2>Student</h2>
-<form action="Student" method="post">
-<input type="text" student="student"><br>
-</form>
+<button type="button">Student</button>
 
 <!--This is the Worker button.-->
 <h2>Worker</h2>
-<form action="Worker" method="post">
-<input type="text" worker="worker"><br> 
-</form>
+<button type="button">Worker</button>
 
 <!--This is the Supervisor/Manager button.-->
 <h2>Supervisor/Manager</h2>
-<form action="Supervisor.Manager" method="post">
-<input type="text" supervisor/manager="supervisor/manager"><br> 
-</form>
+<button type="button">Supervisor/Manager</button>
 
 </body>
 </html>
