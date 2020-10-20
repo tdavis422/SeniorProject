18278ccb7c8802e5be2b430774e6e000a6c3696d
DavidSchmidtSBU <ace.schmidt5@gmail.com>
2020-10-20 17:04:46 -0500

    Update 4 Button Worker and Equipment screen v1.php

--- 4 Button Worker and Equipment screen v1.php
+++ 4 Button Worker and Equipment screen v1.php
@@ -4,27 +4,19 @@
 
 <!--This is the button for Add Worker.-->
 <h2>Add Worker</h2>
-<form action="Add.Worker" method="post">
-<input type="text" add worker="add worker"><br> 
-</form>
+<button type="button">Add Worker</button>
 
 <!--This is the button for Add Equipment.-->
 <h2>Add Equipment</h2>
-<form action="Add.Equipment" method="post">
-<input type="text" add equipment="add equipment"><br> 
-</form>
+<button type="button">Add Equipment</button>
 
 <!--This is the button for Remove Worker.-->
 <h2>Remove Worker</h2>
-<form action="Remove.Worker" method="post">
-<input type="text" remove worker="remove worker"><br> 
-</form>
+<button type="button">Remove Worker</button>
 
 <!--This is the button for Remove Equipment.-->
 <h2>Remove Equipment</h2>
-<form action="Remove.Equipment" method="post">
-<input type="text" remove equipment="remove equipment"><br> 
-</form>
+<button type="button">Remove Equipment</button>
 
 </body>
 </html>
