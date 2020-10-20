f76839157a5cfb4f1a80706c10a9ad9eb0e59c1e
DavidSchmidtSBU <ace.schmidt5@gmail.com>
2020-10-20 17:10:10 -0500

    Update Controller Selection v1.php

--- Controller Selection v1.php
+++ Controller Selection v1.php
@@ -4,54 +4,38 @@
 
 <!--This is the R1 button.-->
 <h2>R1</h2>
-<form action="R1" method="post">
-<input type="text" r1="r1"><br>
-</form>
+<button type="button">R1</button>
 
 <!--This is the R2 button.-->
 <h2>R2</h2>
-<form action="R2" method="post">
-<input type="text" r2="r2"><br> 
-</form>
+<button type="button">R2</button>
 
 <!--This is the R3 button.-->
 <h2>R3</h2>
-<form action="R3" method="post">
-<input type="text" r3="r3"><br> 
-</form>
+<button type="button">R3</button>
 
 <!--This is the R4 button.-->
 <h2>R4</h2>
-<form action="R4" method="post">
-<input type="text" r4="r4"><br> 
-</form>
+<button type="button">R4</button>
 
 <!--This is just text.-->
 <h1>Select A Controller</h1>
 
 <!--This is the B1 button.-->
 <h2>B1</h2>
-<form action="B1" method="post">
-<input type="text" b1="b1"><br> 
-</form>
+<button type="button">B1</button>
 
 <!--This is the B2 button.-->
 <h2>B2</h2>
-<form action="B2" method="post">
-<input type="text" b2="b2"><br> 
-</form>
+<button type="button">B2</button>
 
 <!--This is the B3 button.-->
 <h2>B3</h2>
-<form action="B3" method="post">
-<input type="text" b3="b3"><br> 
-</form>
+<button type="button">B3</button>
 
 <!--This is the B4 button.-->
 <h2>B4</h2>
-<form action="B4" method="post">
-<input type="text" b4="b4"><br> 
-</form>
+<button type="button">B4</button>
 
 </body>
 </html>
