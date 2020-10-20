5f8b68d7e9b93f56504ab35eaf1159a0b8dd0761
DavidSchmidtSBU <ace.schmidt5@gmail.com>
2020-10-20 17:01:40 -0500

    Update 2 Button Screen v1.php

--- 2 Button Screen v1.php
+++ 2 Button Screen v1.php
@@ -4,15 +4,11 @@
 
 <!--This is the button for viewing the equipment that needs sanitized.-->
 <h2>View Equipment Needing Sanitized</h2>
-<form action="View.Equipment.Needing.Sanitized" method="post">
-<input type="text" sanitized="sanitized"><br> 
-</form>
+<button type="button">Sanitize</button>
 
 <!--This is the button for labeling an item as damaged.-->
 <h2>Label Item As Damaged</h2>
-<form action="Label.Item.As.Damaged" method="post">
-<input type="text" damaged="damaged"><br> 
-</form>
+<button type="button">Damaged</button>
 
 </body>
 </html>
