66cea31d636304d5ca0c2ea3efa813984df010e7
DavidSchmidtSBU <ace.schmidt5@gmail.com>
2020-10-21 17:12:27 -0500

    Update Worker List v1.php

--- Worker List v1.php
+++ Worker List v1.php
@@ -6,9 +6,9 @@
 <h2>List of Workers Currently In System</h2>
 
 <ul style="list-style-type:none;">
-  <li>Worker</li>  # This would have the name of the worker.
-  <li>Worker</li>  # This would have the name of the worker.
-  <li>Worker</li>  # This would have the name of the worker.
+  <li>Worker</li>  <!--This would have the name of the worker.-->
+  <li>Worker</li>  <!--This would have the name of the worker.-->
+  <li>Worker</li>  <!--This would have the name of the worker.-->
 </ul>
 
 </body>
