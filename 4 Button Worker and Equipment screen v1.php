ed8184033058efce1d1fcb38e1dff0769fb542a2
DavidSchmidtSBU <ace.schmidt5@gmail.com>
2020-10-21 16:45:01 -0500

    Update 4 Button Worker and Equipment screen v1.php

--- 4 Button Worker and Equipment screen v1.php
+++ 4 Button Worker and Equipment screen v1.php
@@ -3,19 +3,15 @@
 <body>
 
 <!--This is the button for Add Worker.-->
-<h2>Add Worker</h2>
 <button type="button">Add Worker</button>
 
 <!--This is the button for Add Equipment.-->
-<h2>Add Equipment</h2>
 <button type="button">Add Equipment</button>
 
 <!--This is the button for Remove Worker.-->
-<h2>Remove Worker</h2>
 <button type="button">Remove Worker</button>
 
 <!--This is the button for Remove Equipment.-->
-<h2>Remove Equipment</h2>
 <button type="button">Remove Equipment</button>
 
 </body>
