563605d389d6c8160b047f0dfff1b4772f1c30b0
DavidSchmidtSBU <ace.schmidt5@gmail.com>
2020-10-21 16:54:40 -0500

    Update Item Report Selection v1.php

--- Item Report Selection v1.php
+++ Item Report Selection v1.php
@@ -6,27 +6,21 @@
 <h2>What items would you like a report on?</h2>
 
 <!--This is the Ping Pong button.-->
-<h2>Ping Pong</h2>
 <button type="button">Ping Pong</button>
 
 <!--This is the Pool button.-->
-<h2>Pool</h2>
 <button type="button">Pool</button>
 
 <!--This is the Foosball button.-->
-<h2>Foosball</h2>
 <button type="button">Foosball</button>
 
 <!--This is the Xbox button.-->
-<h2>Xbox</h2>
 <button type="button">Xbox</button>
 
 <!--This is the PC button.-->
-<h2>PC</h2>
 <button type="button">PC</button>
 
 <!--This is the With Friends button.-->
-<h2>With Friends</h2>
 <button type="button">With Friends</button>
 
 </body>
