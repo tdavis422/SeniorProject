35eaa9d65207558b2f8a24404754d15c54456f6e
DavidSchmidtSBU <ace.schmidt5@gmail.com>
2020-10-21 17:14:53 -0500

    Update Yes or No Screen v1.php

--- Yes or No Screen v1.php
+++ Yes or No Screen v1.php
@@ -3,18 +3,15 @@
 <body>
 
 <!--This is the button for Go back.-->
-<h3>Go back</h3>
 <button type="button">Go back</button>
 
 <!--This is just text.-->
 <h2>Has the item been sanitized and checked for damage?</h2>
 
 <!--This is the button for Yes.-->
-<h2>Yes</h2>
 <button type="button">Yes</button>
 
 <!--This is the button for No.-->
-<h2>No</h2>
 <button type="button">No</button>
 
 </body>
