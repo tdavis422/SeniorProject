43ad074185bc0ca96b008bdb3ec7aadb3f9b5356
DavidSchmidtSBU <ace.schmidt5@gmail.com>
2020-10-21 17:08:27 -0500

    Update Login screen v1.php

--- Login screen v1.php
+++ Login screen v1.php
@@ -3,15 +3,12 @@
 <body>
 
 <!--This is the Student button.-->
-<h2>Student</h2>
 <button type="button">Student</button>
 
 <!--This is the Worker button.-->
-<h2>Worker</h2>
 <button type="button">Worker</button>
 
 <!--This is the Supervisor/Manager button.-->
-<h2>Supervisor/Manager</h2>
 <button type="button">Supervisor/Manager</button>
 
 </body>
