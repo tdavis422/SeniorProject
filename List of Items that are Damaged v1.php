34d551bd8d234d54b46245882aba26effeb5a130
DavidSchmidtSBU <ace.schmidt5@gmail.com>
2020-10-21 16:57:59 -0500

    Update List of Items that are Damaged v1.php

--- List of Items that are Damaged v1.php
+++ List of Items that are Damaged v1.php
@@ -6,9 +6,9 @@
 <h2>List of Items That Are Damaged</h2>
 
 <ul style="list-style-type:none;">
-  <li>Equipment</li>  # This would have the name of the piece of equipment.
-  <li>Equipment</li>  # This would have the name of the piece of equipment.
-  <li>Equipment</li>  # This would have the name of the piece of equipment.
+  <li>Equipment</li>  <!--This would have the name of the piece of equipment.-->
+  <li>Equipment</li>  <!--This would have the name of the piece of equipment.-->
+  <li>Equipment</li>  <!--This would have the name of the piece of equipment.-->
 </ul>
 
 </body>
