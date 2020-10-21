ffa07401357b345a0f45f0c77e171e8de29bb4fc
DavidSchmidtSBU <ace.schmidt5@gmail.com>
2020-10-21 16:57:23 -0500

    Update List of Items Needing Sanitizing v1.php

--- List of Items Needing Sanitizing v1.php
+++ List of Items Needing Sanitizing v1.php
@@ -6,9 +6,9 @@
 <h2>List of Items Needing Santizied</h2>
 
 <ul style="list-style-type:none;">
-  <li>Equipment</li>  # This would have the name of the piece of equipment.
-  <li>Equipment</li>  # This would have the name of the piece of equipment.
-  <li>Equipment</li>  # This would have the name of the piece of equipment.
+  <li>Equipment</li>  <!--This would have the name of the piece of equipment.-->
+  <li>Equipment</li>  <!--This would have the name of the piece of equipment.-->
+  <li>Equipment</li>  <!--This would have the name of the piece of equipment.-->
 </ul>
 
 </body>
