04537ef2926fb80815c71585e9089c41239d6cbf
DavidSchmidtSBU <ace.schmidt5@gmail.com>
2020-10-21 16:48:05 -0500

    Update Controller Selection v1.php

--- Controller Selection v1.php
+++ Controller Selection v1.php
@@ -3,38 +3,30 @@
 <body>
 
 <!--This is the R1 button.-->
-<h2>R1</h2>
 <button type="button">R1</button>
 
 <!--This is the R2 button.-->
-<h2>R2</h2>
 <button type="button">R2</button>
 
 <!--This is the R3 button.-->
-<h2>R3</h2>
 <button type="button">R3</button>
 
 <!--This is the R4 button.-->
-<h2>R4</h2>
 <button type="button">R4</button>
 
 <!--This is just text.-->
 <h1>Select A Controller</h1>
 
 <!--This is the B1 button.-->
-<h2>B1</h2>
 <button type="button">B1</button>
 
 <!--This is the B2 button.-->
-<h2>B2</h2>
 <button type="button">B2</button>
 
 <!--This is the B3 button.-->
-<h2>B3</h2>
 <button type="button">B3</button>
 
 <!--This is the B4 button.-->
-<h2>B4</h2>
 <button type="button">B4</button>
 
 </body>
