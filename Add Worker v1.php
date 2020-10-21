7734d55b9bb9abb4f238cac0982972df341266e3
DavidSchmidtSBU <ace.schmidt5@gmail.com>
2020-10-21 16:46:01 -0500

    Update Add Worker v1.php

--- Add Worker v1.php
+++ Add Worker v1.php
@@ -6,11 +6,9 @@
 <h2>Would you like to add this worker?</h2>
 
 <!--This is the button for Yes.-->
-<h2>Yes</h2>
 <button type="button">Yes</button>
 
 <!--This is the button for No.-->
-<h2>No</h2>
 <button type="button">No</button>
 
 </body>
