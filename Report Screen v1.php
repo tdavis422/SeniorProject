3e466e9ba303b4ab14aa0c77b99052c8816e2ca2
DavidSchmidtSBU <ace.schmidt5@gmail.com>
2020-10-21 17:10:04 -0500

    Update Report Screen v1.php

--- Report Screen v1.php
+++ Report Screen v1.php
@@ -6,11 +6,9 @@
 <h2>Would you like to create a report for all items or specific items?</h2>
 
 <!--This is the button for All Items.-->
-<h2>All Items</h2>
 <button type="button">All Items</button>
 
 <!--This is the button for Specific Item.-->
-<h2>Specific Item</h2>
 <button type="button">Specific Item</button>
 
 </body>
