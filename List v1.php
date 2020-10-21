af9200838d8b96500e5b9766836d017ee42015bc
DavidSchmidtSBU <ace.schmidt5@gmail.com>
2020-10-21 17:07:18 -0500

    Update List v1.php

--- List v1.php
+++ List v1.php
@@ -1,38 +1,36 @@
 <!DOCTYPE html>
 <html>
 <body>
-<?php
 
-# This is just the starter code for all of the lists. There could be more or less than three items depending on which list you are in. 
+<!--This is just the starter code for all of the lists. There could be more or less than three items depending on which list you are in.--> 
 
-# This is the code for listing equipment needing to be sanitized.
+<!--This is the code for listing equipment needing to be sanitized.-->
 <ul style="list-style-type:none;">
-  <li>Equipment</li>  # This would have the name of the piece of equipment.
-  <li>Equipment</li>  # This would have the name of the piece of equipment.
-  <li>Equipment</li>  # This would have the name of the piece of equipment.
+  <li>Equipment</li>  <!--This would have the name of the piece of equipment.-->
+  <li>Equipment</li>  <!--This would have the name of the piece of equipment.-->
+  <li>Equipment</li>  <!--This would have the name of the piece of equipment.-->
 </ul>
 
-# This is the code for listing equipment that is damaged.
+<!--This is the code for listing equipment that is damaged.-->
 <ul style="list-style-type:none;">
-  <li>Equipment</li>  # This would have the name of the piece of equipment.
-  <li>Equipment</li>  # This would have the name of the piece of equipment.
-  <li>Equipment</li>  # This would have the name of the piece of equipment.
+  <li>Equipment</li>  <!--This would have the name of the piece of equipment.-->
+  <li>Equipment</li>  <!--This would have the name of the piece of equipment.-->
+  <li>Equipment</li>  <!--This would have the name of the piece of equipment.-->
 </ul>
 
-# This is the code for listing workers that are currently in the system.
+<!--This is the code for listing workers that are currently in the system.-->
 <ul style="list-style-type:none;">
-  <li>Worker</li>  # This would have the name of the worker.
-  <li>Worker</li>  # This would have the name of the worker.
-  <li>Worker</li>  # This would have the name of the worker.
+  <li>Worker</li>  <!--This would have the name of the piece of equipment.-->
+  <li>Worker</li>  <!--This would have the name of the piece of equipment.-->
+  <li>Worker</li>  <!--This would have the name of the piece of equipment.-->
 </ul>
 
-# This is the code for listing all of the equipment.
+<!--This is the code for listing all of the equipment.-->
 <ul style="list-style-type:none;">
-  <li>Equipment</li>  # This would have the name of the piece of equipment.
-  <li>Equipment</li>  # This would have the name of the piece of equipment.
-  <li>Equipment</li>  # This would have the name of the piece of equipment.
+  <li>Equipment</li>  <!--This would have the name of the piece of equipment.-->
+  <li>Equipment</li>  <!--This would have the name of the piece of equipment.-->
+  <li>Equipment</li>  <!--This would have the name of the piece of equipment.-->
 </ul>
 
-?>
 </body>
 </html>
