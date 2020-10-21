bab78ac5495d8cdcf7a98646fc6d43d1b3417f61
DavidSchmidtSBU <ace.schmidt5@gmail.com>
2020-10-21 17:10:43 -0500

    Update Sanitized Or Damaged v1.php

--- Sanitized Or Damaged v1.php
+++ Sanitized Or Damaged v1.php
@@ -6,11 +6,9 @@
 <h2>Does equipment need sanitized or has it been damaged?</h2>
 
 <!--This is the button for sanitize.-->
-<h2>Sanitize</h2>
 <button type="button">Sanitize</button>
 
 <!--This is the button for damaged.-->
-<h2>Damaged</h2>
 <button type="button">Damaged</button>
 
 </body>
