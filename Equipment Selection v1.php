c4278e070ec07beab43c5f25286a5fbb4f5ea62c
DavidSchmidtSBU <ace.schmidt5@gmail.com>
2020-10-21 16:49:53 -0500

    Update Equipment Selection v1.php

--- Equipment Selection v1.php
+++ Equipment Selection v1.php
@@ -3,31 +3,24 @@
 <body>
 
 <!--This is the Ping Pong button.-->
-<h2>Ping Pong</h2>
 <button type="button">Ping Pong</button>
 
 <!--This is the Pool button.-->
-<h2>Pool</h2>
 <button type="button">Pool</button>
 
 <!--This is the Foosball button.-->
-<h2>Foosball</h2>
 <button type="button">Foosball</button>
 
 <!--This is the Xbox button.-->
-<h2>Xbox</h2>
 <button type="button">Xbox</button>
 
 <!--This is the PC button.-->
-<h2>PC</h2>
 <button type="button">PC</button>
 
 <!--This is the With Friends button.-->
-<h2>With Friends</h2>
 <button type="button">With Friends</button>
 
 <!--This is the Check In button.-->
-<h1>Check In</h1>
 <button type="button">Check In</button>
 
 </body>
