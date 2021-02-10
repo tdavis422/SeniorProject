969d955a946fcbb55b6a3f67d22c403b1e9bc678
Treyton Davis <37282419+tdavis422@users.noreply.github.com>
2021-02-10 13:08:37 -0600

    Update db_script.sql
    Edited this file to be easier to read in case of errors happening

--- Database/db_script.sql
+++ Database/db_script.sql
@@ -7,16 +7,33 @@ PRAGMA foreign_keys = off;
 BEGIN TRANSACTION;
 
 -- Table: checkouts
-CREATE TABLE checkouts (checkoutsID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, equipmentID INTEGER REFERENCES equipment (equipmentID) MATCH SIMPLE NOT NULL, studentID INTEGER NOT NULL, workerID INTEGER REFERENCES workers (workerID) MATCH SIMPLE NOT NULL, timeOut DATETIME NOT NULL, timeIn DATETIME, notes STRING (255));
+CREATE TABLE checkouts (checkoutsID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
+	equipmentID INTEGER REFERENCES equipment (equipmentID) MATCH SIMPLE NOT NULL, 
+	studentID INTEGER NOT NULL, 
+	workerID INTEGER REFERENCES workers (workerID) MATCH SIMPLE NOT NULL, 
+	timeOut DATETIME NOT NULL, 
+	timeIn DATETIME, 
+	notes STRING (255));
 
 -- Table: equipment
-CREATE TABLE equipment (equipmentID INTEGER PRIMARY KEY ASC, equipmentType INTEGER NOT NULL REFERENCES equipmentTypes (equipmentSerialCode), condition STRING NOT NULL, lastCleanedBy INTEGER NOT NULL REFERENCES workers (workerID) MATCH SIMPLE, dateAdded DATE NOT NULL, dateRemoved DATE NOT NULL, notes STRING (255));
+CREATE TABLE equipment (equipmentID INTEGER PRIMARY KEY ASC, 
+	equipmentType INTEGER NOT NULL REFERENCES equipmentTypes (equipmentSerialCode), 
+	condition STRING NOT NULL, 
+	lastCleanedBy INTEGER NOT NULL REFERENCES workers (workerID) MATCH SIMPLE, 
+	dateAdded DATE NOT NULL, 
+	dateRemoved DATE NOT NULL, 
+	notes STRING (255));
 
 -- Table: equipmentTypes
-CREATE TABLE equipmentTypes (equipmentTypeID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, equipmentType STRING (255) NOT NULL);
+CREATE TABLE equipmentTypes (equipmentTypeID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
+	equipmentType STRING (255) NOT NULL);
 
 -- Table: workers
-CREATE TABLE workers (workerID PRIMARY KEY ASC NOT NULL, firstName STRING (255) NOT NULL, lastName STRING (255) NOT NULL, type STRING (255) NOT NULL, password STRING NOT NULL);
+CREATE TABLE workers (workerID PRIMARY KEY ASC NOT NULL, 
+	firstName STRING (255) NOT NULL, 
+	lastName STRING (255) NOT NULL, 
+	type STRING (255) NOT NULL, 
+	password STRING NOT NULL);
 
 -- Trigger: statusChangeNeedsSanitized
 CREATE TRIGGER statusChangeNeedsSanitized AFTER UPDATE OF timeIn ON checkouts FOR EACH ROW BEGIN SELECT * FROM equipment;
