4de4417abf69d276a1e0ba2eb607d0a77c9506cc
ChaseLaw <38270498+ChaseLaw@users.noreply.github.com>
2021-02-12 08:39:10 -0600

    WIP database file

--- Database/db_script.sql
+++ Database/db_script.sql
@@ -1,51 +1,47 @@
---
--- File generated with SQLiteStudio v3.2.1 on Sat Feb 6 13:59:32 2021
---
--- Text encoding used: System
---
-PRAGMA foreign_keys = off;
-BEGIN TRANSACTION;
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "-06:00";
 
 -- Table: checkouts
-CREATE TABLE checkouts (
-	checkoutsID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
-	equipmentID INTEGER REFERENCES equipment (equipmentID) MATCH SIMPLE NOT NULL, 
-	studentID INTEGER NOT NULL, 
-	workerID INTEGER REFERENCES workers (workerID) MATCH SIMPLE NOT NULL, 
-	timeOut DATETIME NOT NULL, 
-	timeIn DATETIME, 
-	notes STRING (255)
-);
+CREATE TABLE `checkouts` (
+	`checkoutsID` INT PRIMARY KEY AUTO_INCREMENT NOT NULL, 
+	`equipmentID` INT REFERENCES equipment (equipmentID) MATCH SIMPLE NOT NULL, 
+	`studentID` INT NOT NULL, 
+	`workerID` INT REFERENCES workers (workerID) MATCH SIMPLE NOT NULL, 
+	`timeOut` DATETIME NOT NULL, 
+	`timeIn` DATETIME, 
+	`notes` VARCHAR (255)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Table: equipment
-CREATE TABLE equipment (
-	equipmentID INTEGER PRIMARY KEY ASC, 
-	equipmentType INTEGER NOT NULL REFERENCES equipmentTypes (equipmentSerialCode), 
-	condition STRING NOT NULL, 
-	lastCleanedBy INTEGER NOT NULL REFERENCES workers (workerID) MATCH SIMPLE, 
-	dateAdded DATE NOT NULL, 
-	dateRemoved DATE NOT NULL, 
-	notes STRING (255)
-);
+CREATE TABLE `equipment` (
+	`equipmentID` INT PRIMARY KEY ASC, 
+	`equipmentType` INT NOT NULL REFERENCES equipmentTypes (equipmentSerialCode), 
+	`condition VARCHAR` NOT NULL, 
+	`lastCleanedBy` INT NOT NULL REFERENCES workers (workerID) MATCH SIMPLE, 
+	`dateAdded` DATE NOT NULL, 
+	`dateRemoved` DATE NOT NULL, 
+	`notes` VARCHAR (255)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Table: equipmentTypes
-CREATE TABLE equipmentTypes (
-	equipmentTypeID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
-	equipmentType STRING (255) NOT NULL
-);
+CREATE TABLE `equipmentTypes` (
+	`equipmentTypeID` INT PRIMARY KEY AUTO_INCREMENT NOT NULL, 
+	`equipmentType` VARCHAR (255) NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Table: workers
-CREATE TABLE workers (
-	workerID PRIMARY KEY ASC NOT NULL, 
-	firstName STRING (255) NOT NULL, 
-	lastName STRING (255) NOT NULL, 
-	type STRING (255) NOT NULL, 
-	password STRING NOT NULL
-);
+CREATE TABLE `workers` (
+	`workerID` PRIMARY KEY ASC NOT NULL, 
+	`firstName` VARCHAR (255) NOT NULL, 
+	`lastName` VARCHAR (255) NOT NULL, 
+	`type` VARCHAR (255) NOT NULL, 
+	`password` VARCHAR NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Trigger: statusChangeNeedsSanitized
 CREATE TRIGGER statusChangeNeedsSanitized AFTER UPDATE OF timeIn ON checkouts FOR EACH ROW BEGIN SELECT * FROM equipment;
 UPDATE condition SET condition = 'needsSanitized' WHERE checkouts.equipmentID = equipment.equipmentID; END;
 
-COMMIT TRANSACTION;
-PRAGMA foreign_keys = on;
+COMMIT;-- TRANSACTION;
