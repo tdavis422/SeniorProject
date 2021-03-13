859d66398a4d065febbd64906f179b1cc4f101e2
DavidSchmidtSBU <ace.schmidt5@gmail.com>
2021-03-13 17:37:53 -0600

    Revert "Added reports"
    This reverts commit 90b33c0cf02685beaf0ed02a819c6c481eeb3792.

---
+++ cms-master/admin/includes/Add Report (All).php
@@ -0,0 +1,52 @@
+<?php include "includes/admin_header.php" ?>
+<?php include "functions.php" ?>
+
+<div id="wrapper">
+
+<?php include "includes/admin_navigation.php" ?>
+
+  <div id="page-wrapper">
+    <div class="container-fluid">
+      <!-- Page Heading -->
+      <div class="row">
+        <div class="col-lg-12">
+          <h1 class="page-header">
+            Welcome to Admin
+            <small><?=$_SESSION['username']?></small>
+          </h1>
+<?php
+if(isset($_GET['source'])){
+  $source = $_GET['source'];
+}else{
+  $source = '';
+}
+
+switch($source){
+  case 'add_equipment':
+    include "includes/add_equipment.php";
+    break;
+  case 'sanitize':
+	include "includes/sanitize_equipment.php";
+	break;
+  case 'edit':
+    include "includes/edit_equipment.php";
+    break;
+  case 'remove':
+	  include "includes/remove_equipment.php";
+	  break;
+  case 'add_type':
+    include "includes/add_equipment_type.php";
+    break;
+  default:
+    include "includes/view_all_equipment.php";
+    break;
+}
+?>
+        </div>
+      </div>
+            <!-- /.container-fluid -->
+    </div>
+        <!-- /#page-wrapper -->
+</div>
+    <!-- /#wrapper -->
+<?php include "includes/admin_footer.php" ?>
