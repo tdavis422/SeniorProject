59ca63aa4e0921e2ab38f9c8aa55a7bdbdb365a9
ChaseLaw <38270498+ChaseLaw@users.noreply.github.com>
2021-03-16 00:12:41 -0500

    Update cleardatabase.php

--- cms-master/admin/cleardatabase.php
+++ cms-master/admin/cleardatabase.php
@@ -7,37 +7,52 @@
 
   <div id="page-wrapper">
     <div class="container-fluid">
-      <!-- Page Heading -->
-      <div class="row">
-        <div class="col-lg-12">
-          <h1 class="page-header">
-            Do you want to clear the database?
-            <small><?=$_SESSION['username']?></small>
-          </h1>
-		  <script>
-			function myFunction()
-			{
-				var txt;
-				if(confirm("Are you sure you want to clear the checkouts of the database?"))
+		<!-- Page Heading -->
+		<div class="row">
+			<div class="col-lg-12">
+				<h1 class="page-header">
+					Do you want to clear the database?
+					<small><?=$_SESSION['username']?></small>
+				</h1>
+			
+			
+			<script language="javascript">
+				function clearData()
 				{
-					<?php></?>
-					alert("Check-ins have be cleared and redirecting to Admin Dashboard")
-					window.location.href = "index.php";
-				} else
+					var a;
+					if(confirm("Are you sure you want to clear the checkouts of the database?"))
+					{
+						<?php
+							$txt1 = "Here";
+							//$pdo = new PDO('mysql:host=localhost;dbname=cms', 'root', '');
+							$sql = "TRUNCATE TABLE 'checkouts'"; 
+							mysql_query($connection, $sql);
+							//$stmt = $connection->prepare($sql);
+							//echo "<h2>" . $txt1 . "</h2>";
+							//$stmt->execute($stmt);
+						?>
+						alert("Check-ins have be cleared and redirecting to Admin Dashboard")
+						//window.location.href = "index.php";
+					} else
+					{
+						alert("Redirecting to Admin Dashboard")
+						window.location.href = "index.php";
+					}
+				}
+				
+				function ref()
 				{
 					alert("Redirecting to Admin Dashboard")
 					window.location.href = "index.php";
 				}
-			}
-		  </script>
-		  
-		  
-		  
-			<button type="button" onclick="myFunction()">Yes</button>
+			</script>
+		
+			<button type="button" name="confirm" onclick="clearData()">Yes</button>
+			<button type="button" name="deny" onclick="ref()">No</button>
 			
-			<a href= "index.php">
-			<button>No</button>
-<?php>
+			</div>	
+			
+		</div>
 
 
 
