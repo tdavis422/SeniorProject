580eeb2b8a2d5066ed2bc366dee39a92328a38c2
DavidSchmidtSBU <ace.schmidt5@gmail.com>
2021-04-06 14:54:40 -0500

    Placeholder changes for all and specific report files

--- cms-master/admin/includes/createAllReport.php
+++ cms-master/admin/includes/createAllReport.php
@@ -23,4 +23,57 @@
 
             <a href= "report.php">
 			<button>No</button>
-            
+
+<!<table class="table table-bordered table-hover">
+  <!<thead>
+    <!<tr>
+      <!<th>Id<!</th>
+      <!<th>Username<!</th>
+      <!<th>Firstname<!</th>
+      <!<th>Lastname<!</th>
+      <!<th>Email<!</th>
+      <!<th>Role<!</th>
+    <!</tr>
+  <!</thead>
+  <!<tbody>
+
+<!<tr>
+  <!<td><<!?= $user_id ?><<!/td>
+  <!<td><<!?= $username ?><<!/td>
+  <!<td><<!?= $user_firstname ?><!/td>
+  <!<td><<!?= $user_lastname ?><!</td>
+  <!<td><<!?= $user_email ?><!</td>
+  <!<td><<!?= $user_role ?><!</td>
+  <!<td><<!a href='users.php?change_to_admin=<<!?= $user_id ?>'>Admin<<!/a><<!/td>
+  <!<td><<!a href='users.php?change_to_worker=<<!?= $user_id ?>'>Worker<<!/a><<!/td>
+  <!<td><<!a href='users.php?source=edit_user&edit_user=<<!?= $user_id ?>'>Edit<<!/a><<!/td>
+  <!<td><<!a href='users.php?delete=<<!?= $user_id ?>'>Delete<<!/a><<!/td>
+<!</tr>
+<<!?php
+}
+<!?>
+  <!</tbody>
+<!</table>
+
+<<!?php
+if(isset($_GET['change_to_admin'])){
+  $the_user_id = $_GET['change_to_admin'];
+  $query = "UPDATE users SET user_role = 'admin' WHERE user_id = {$the_user_id}";
+  $change_role_query = mysqli_query($connection, $query);
+  header("Location: users.php");
+}
+
+if(isset($_GET['change_to_worker'])){
+  $the_user_id = $_GET['change_to_worker'];
+  $query = "UPDATE users SET user_role = 'worker' WHERE user_id = {$the_user_id}";
+  $change_role_query = mysqli_query($connection, $query);
+  header("Location: users.php");
+}
+
+if(isset($_GET['delete'])){
+  $the_user_id = $_GET['delete'];
+  $query = "DELETE FROM users WHERE user_id = {$the_user_id} ";
+  $delete_user_query = mysqli_query($connection, $query);
+  header("Location: users.php");
+}
+?>       
