0c5fe3e9b2bfa77364753b2132b82d6e27edd953
ChaseLaw <38270498+ChaseLaw@users.noreply.github.com>
2021-04-08 16:22:25 -0500

    added comments

--- cms-master/admin/cleardatabase.php
+++ cms-master/admin/cleardatabase.php
@@ -17,57 +17,61 @@
 					<small><?=$_SESSION['username']?></small>
 				</h1>
 			<script language="javascript">
+				//delares the function fofr clearing the data
 				function clearData()
 				{
+					//asks if the user wants to clear the database
 					if(confirm("Are you sure you want to clear the checkouts of the database?"))
 					{
 						<?php
+
+							//sets query for clearing out the database and executes query
 							$query = "TRUNCATE TABLE `checkouts`";
 							mysqli_query($connection, $query);
+
+              //doing some error checking
 							$query2 = "SELECT * FROM checkouts";
-							$result = null;
+							$result = "This is not the code you are looking for.";
 							$result = mysqli_query($connection, $query2);
-							//echo "The Result is $result";
+
+              //checking if the database is clear
 							if(isset($result))
 							{
 						?>
-							
-								alert("Emptied the checkouts table and redirecting to dashboard"); 
-								window.location.replace("index.php");	
-						<?php	
-							}
-							else 
+                //if the database table is cleared then the user will be redirected to the admin index
+								alert("Emptied the checkouts table and redirecting to dashboard");
+								window.location.replace("index.php");
+						<?php
+              }//if
+							else
 							{
 						?>
-									alert("Database was not able to empty.  Redirecting to admin page"); 
+                  //if the database was unable to be emptied the system will tell the user and then redirect to the admin index
+									alert("Database was not able to empty.  Redirecting to admin page");
 									window.location.replace("index.php");
-								
+
 						<?php
-							}
+              }//else
 						?>
-					}
+					}//if
 
-          window.location.replace("index.php");
-
-				}
+				}//cleardata()
 
 				function ref()
 				{
+          //redirects to the admin index
 					alert("Redirecting to Admin Dashboard");
 					window.location.replace("index.php");
-				}
+				}//ref()
 			</script>
-			
-			<h2>
-			
-			</h2>
-			
+
+      <!-- a button that is used to clear the database-->
 			<button type="button" class="btn btn-danger" onclick="clearData()">Yes</button>
-				
-			
+
+      <!-- a button that will redirect to the admin index -->
 			<button type="button" class="btn btn-success" onclick="ref()">No</button>
-			
+
 			</div>
 		</div>
-		
+
 <?php include "includes/admin_footer.php" ?>
